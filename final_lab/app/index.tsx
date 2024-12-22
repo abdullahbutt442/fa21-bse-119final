cbd3e5435452bdd7540690406df3f8f8ee2b89b6
Abdullah Aasir <abdullahaasir402@gmail.com>
2024-12-22 20:46:40 +0500

    Updated

---
+++ final_lab/app/index.tsx
@@ -0,0 +1,15 @@
+
+import { Text, View } from "react-native"
+
+export default function Home()
+{
+  return(
+    <>
+       <View>
+        <Text>
+          Hello world 
+        </Text>
+       </View>
+    </>
+  )
+}
